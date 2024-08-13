b0e6f6d47d72580842cfcdbbe7ceeca38c42e3b9
Wander Jerez <wanderroque01@gmail.com>
2024-08-13 19:05:44 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const tasks = [];
+let time = 0;
+let timer = null;
+let timerBreak = null;
+let current = null;
+
+const bAdd = document.getElementById("bAdd");
+const itTask = document.getElementById("itTask");
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (itTask.value !== "") {
+    createTask(itTask.value);
+    itTask.value = "";
+    renderTasks();
+  }
+});
+
+function createTask(value) {
+  const newTask = {
+    id: (Math.random() * 100).toString(36).slice(3),
+    title: value,
+    completed: false,
+  };
+
+  tasks.unshift(newTask);
+}
+
+function renderTasks() {
+  const html = tasks.map((task) => {
+    return `
+            <div class="task">
+                <div class="completed">${
+                  task.completed
+                    ? `<span class="done">Done</span>`
+                    : `<button class="start-button" data-id="${task.id}">Start</button>`
+                }</div>
+                <div class="title">${task.title}</div>
+            </div>
+        `;
+  });
+  const tasksContainer = document.querySelector("#tasks");
+  tasksContainer.innerHTML = html.join("");
+}
